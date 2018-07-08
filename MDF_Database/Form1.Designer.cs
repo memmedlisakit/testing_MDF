c5807b3de64b8c70d02b8be0fa02bb7da85d5b3f
memmedlisakit <memmedlisakit@gmail.com>
2018-07-08 12:32:54 +0400

    MDF

---
+++ MDF_Database/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MDF_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(255, 57);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(193, 240);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(210, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "With MDF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 363);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MDF_Database/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Data.SQLite;
+
+namespace MDF_Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string connection = "Data Source="+getPath()+ @"App_Data\SQLiteDB.db;Version=3;";
+
+         
+
+
+        public static string getPath()
+        {
+            string path = Application.StartupPath;
+
+            List<string> splited = Regex.Split(path, "bin").ToList();
+
+            return splited[0];
+
+        }
+
+
+
+        public void fillAll()
+        {
+            this.textBox3.Text = "";
+            SQLiteDataAdapter da = new SQLiteDataAdapter();
+            DataTable dt = new DataTable();
+            string SQLite = "SELECT * FROM [Users]";
+            SQLiteConnection con = new SQLiteConnection(connection);
+            SQLiteCommand com = new SQLiteCommand(SQLite, con);
+            da.SelectCommand = com;
+            da.Fill(dt);
+            foreach (DataRow item in dt.Rows)
+            {
+                this.textBox3.Text += item["name"] + " -- " + item["surname"] + "\r\n";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var name = textBox1.Text;
+            var surname = textBox2.Text;
+
+            SQLiteConnection con = new SQLiteConnection(connection);
+            con.Open();
+            string SQLite = "INSERT INTO [Users](name, surname) VALUES('" + name + "', '" + surname + "')";
+
+            SQLiteCommand com = new SQLiteCommand(SQLite, con);
+            int effected = com.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(effected + "");
+            this.fillAll();
+        }
+    }
+}
